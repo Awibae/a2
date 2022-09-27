38fc9e2b82796f42f12c62f3b6bb670510b2ba2e
AlexBanigan <abanigan@myseneca.ca>
2022-09-27 16:35:40 -0400

    end of class commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+app.use(express.static('public'));
+var path = require("path");
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+function onHttpStart(){
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+})
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "/views/about.html"));
+})
+
+
+app.listen(HTTP_PORT, onHttpStart);

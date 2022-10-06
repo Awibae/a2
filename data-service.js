51a969a927eec972727d62172b57e7f9f69cfff4
Alex Banigan <alexbanigan2002@gmail.com>
2022-10-06 19:17:48 -0400

    i dont even know anymore man

---
+++ data-service.js
@@ -0,0 +1,34 @@
+var employees = []
+var departments = [];
+
+var fs = require('fs');
+const { resolve } = require('path');
+
+function initialize() {
+    var readSuccess = false;
+    return new Promise(function(resolve, reject) {
+        fs.readFile('./data/employees.json', (err,data) => {
+            if (err) {
+                readSuccess = false;
+                reject("Failure to read file employees.json");
+            }
+            else {
+                readSuccess = true;
+                employees = json.parse(data);
+            }
+        })
+        fs.readFile('./data/departments.json',(err,data) => {
+            if (err) {
+                readSuccess = false;
+                reject("Failure to read file employees.json!");
+            }
+            else {
+                readSuccess = true;
+                employees = JSON.parse(data);
+            }
+        })
+        if (readSuccess == true) {
+            resolve(employees.includes(isManager == true));
+        }
+    })
+}

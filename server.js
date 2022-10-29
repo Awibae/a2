d3b8a4db0e5d262b03958ca0734cb55549b5b425
Alex Banigan <alexbanigan2002@gmail.com>
2022-10-29 16:54:07 -0400

    starting a3

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 /*************************************************************************
 * BTI325– Assignment 2
-* I declare that this assignment is my own work in accordance with Seneca Academic
-Policy. No part * of this assignment has been copied manually or electronically from any
-other source
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+* No part of this assignment has been copied manually or electronically from any other source
 * (including 3rd party web sites) or distributed to other students.
 *
-* Name: Alexander Banigan Student ID: 151167202 Date: 2022-10-11
+* Name: Alexander Banigan Student ID: 151167202 Date: 2022-10-29
 *
 * Your app’s URL (from Cyclic) : https://zany-top-coat-elk.cyclic.app
 *
@@ -15,6 +14,19 @@ var express = require("express");
 var app = express();
 app.use(express.static('public'));
 var path = require("path");
+var multer = require("multer");
+var fs = require('fs');
+
+var images = [];
+
+const storage = multer.diskStorage({
+    destination: "./public/images/uploaded",
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+})
+
+const upload = multer({storage : storage});
 
 var HTTP_PORT = process.env.PORT || 8080;
 
@@ -25,9 +37,11 @@ function onHttpStart(){
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "/views/home.html"));
 })
+
 app.get("/about", function(req, res){
     res.sendFile(path.join(__dirname, "/views/about.html"));
 })
+
 app.get("/employees", function(req, res) {
     dataservice.getAllEmployees().then(function(employees) {
         res.json(employees);
@@ -35,6 +49,10 @@ app.get("/employees", function(req, res) {
         res.json(error);
     });
 })
+app.get("/employees/add", function(req, res) {
+
+})
+
 app.get("/managers", function(req, res) {
     dataservice.getManagers().then(function(managers) {
         res.json(managers);
@@ -42,6 +60,7 @@ app.get("/managers", function(req, res) {
         res.json(error);
     });
 })
+
 app.get("/departments", function(req, res) {
     dataservice.getDepartments().then(function(departments) {
         res.json(departments);
@@ -49,6 +68,25 @@ app.get("/departments", function(req, res) {
         res.json(error);
     });
 })
+
+
+app.get("/images/add", function(req, res) {
+
+})
+app.post("/images/add", upload.single("imageFile", (req, res) => {
+    res.send("/images");
+}))
+app.get("/images", function(req, res) {
+    fs.readdir("./public/images/uploaded", function(err, items) {
+        if (err) {
+            reject("Failure to read file file");
+        }
+        else {
+            images = JSON.parse(items);
+        }
+    })
+})
+
 app.use((req, res)=> {
     res.status(404).send("Page not found");
 })
